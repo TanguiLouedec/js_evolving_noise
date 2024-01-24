01ebdb7e399d5597a358be0eeabe9de201144f6e
Koffingering <tangui.louedec@gmail.com>
2024-01-24 01:40:35 +0100

    Init: Noise is generated but still static, wip

---
+++ script.js
@@ -0,0 +1,71 @@
+'use strict';
+let perlin = {
+    rand_vect: function(){
+        let theta = Math.random() * 2 * Math.PI;
+        return {x: Math.cos(theta), y: Math.sin(theta)};
+    },
+    dot_prod_grid: function(x, y, vx, vy){
+        let g_vect;
+        let d_vect = {x: x - vx, y: y - vy};
+        if (this.gradients[[vx,vy]]){
+            g_vect = this.gradients[[vx,vy]];
+        } else {
+            g_vect = this.rand_vect();
+            this.gradients[[vx, vy]] = g_vect;
+        }
+        return d_vect.x * g_vect.x + d_vect.y * g_vect.y;
+    },
+    smootherstep: function(x){
+        return 6*x**5 - 15*x**4 + 10*x**3;
+    },
+    interp: function(x, a, b){
+        return a + this.smootherstep(x) * (b-a);
+    },
+    seed: function(){
+        this.gradients = {};
+        this.memory = {};
+    },
+    get: function(x, y) {
+        if (this.memory.hasOwnProperty([x,y]))
+            return this.memory[[x,y]];
+        let xf = Math.floor(x);
+        let yf = Math.floor(y);
+        //interpolate
+        let tl = this.dot_prod_grid(x, y, xf,   yf);
+        let tr = this.dot_prod_grid(x, y, xf+1, yf);
+        let bl = this.dot_prod_grid(x, y, xf,   yf+1);
+        let br = this.dot_prod_grid(x, y, xf+1, yf+1);
+        let xt = this.interp(x-xf, tl, tr);
+        let xb = this.interp(x-xf, bl, br);
+        let v = this.interp(y-yf, xt, xb);
+        this.memory[[x,y]] = v;
+        return v;
+    }
+}
+perlin.seed();
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const grid_size = 4;
+const resolution = 256;
+const colorscale = 250;
+
+let pixel_size = canvas.width / resolution;
+let pixel_num = grid_size / resolution;
+
+for (let i = 0; i < grid_size; i += pixel_num / grid_size) {
+  for (let j = 0; j < grid_size; j+= pixel_num / grid_size) {
+     let v = parseInt(perlin.get(j, i) * colorscale);
+    ctx.fillStyle = 'hsl('+v+',50%,50%)';
+    ctx.fillRect(
+      j / grid_size * canvas.width,
+      i / grid_size * canvas.width,
+      pixel_size,
+      pixel_size
+    );
+  } 
+}
