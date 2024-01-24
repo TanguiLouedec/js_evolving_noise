7432ca2b6449973b7fc19b60f0f2f2d1579d47f1
Koffingering <tangui.louedec@gmail.com>
2024-01-25 00:12:19 +0100

    Update: Changed the noise algorithm, added blur filter and grain

---
+++ grain.js
@@ -0,0 +1,16 @@
+const canvas = document.getElementById('grain');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.fillStyle = 'black';
+
+for (let i = 0; i < canvas.width; i++) {
+  for (let j = 0; j < canvas.height; j++) {
+    let x = Math.random();
+    if (x < 0.1) {
+      ctx.fillRect(i, j, 1, 1);
+    }
+  }
+}
